764e24cf0e1406187aeb92df6a424a51161428ec
Suman Kumar <suman@Jen-MacBook.local>
2018-06-11 00:27:43 +0530

    up to tutorial 3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React, { Component, Fragment } from 'react'
 import {Header, Footer} from './Components/Layouts'
-import Exercise from './Components/Exercises'
+import Exercises from './Components/Exercises';
+import { muscles, exercises} from './Components/store';
 
 
 
-class App extends Component {
+export default class extends Component {
+
+  state = {
+    exercises
+  }
+
+  getExercisesByMuscles(){
+    return Object.entries(this.state.exercises.reduce((exercises, exercise) => {
+      const { muscles } = exercise
+
+      exercises[muscles] = exercises[muscles] ? [...exercises[muscles], exercise] : [exercise]
+
+      return exercises
+
+      }, {}))
+    
+  }
+
   render() {
+
+    const exercises = this.getExercisesByMuscles()
+
     return (
       <Fragment>
+
         <Header />
-          
-          <Exercise />
-          
-        <Footer />
+
+        <Exercises exercises={exercises} />        
+
+        <Footer muscles={muscles} />
+
       </Fragment>
-    );
+    )
   }
 }
-
-export default App;
--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -1,21 +1,22 @@
 import React from 'react';
-// import { Paper, Tabs } from '@material-ui/core';
-// import { Tab } from '@material-ui/core/Tabs';
 
 import {Paper, Tabs} from '@material-ui/core/';
-import {Tab} from '@material-ui/core/';
+import Tab from '@material-ui/core/Tab';
 
 export default props => (
-	<Paper>
-		<Tabs
-			value={0}
-			indicatorColor="primary"
-			textColor="primary"
-			centered
-		>
-			<Tab label="Item One" />
-			<Tab label="Item Two" />
-			<Tab label="Item Three" />
-		</Tabs>
-	</Paper>
+  <Paper>
+    <Tabs
+      value={0}
+      indicatorColor="primary"
+      textColor="primary"
+      centered>
+      
+      <Tab label="All" />
+
+      {props.muscles.map(group => 
+        <Tab label={group}/>
+      )}
+      
+    </Tabs>
+  </Paper>
 );
